e9633d59838e5a088b32d5d289c4b26b7b98ccfd
Keshav Singhania <worksinghania@gmail.com>
2025-08-03 13:53:58 +0530

    Find Number Of even digits elements present inside a given array

---
+++ src/day4_LinearAlgoArray/noOFEven.java
@@ -0,0 +1,31 @@
+package day4_LinearAlgoArray;
+
+//to find to number of even digits elements present inside an given array
+public class noOFEven {
+    public static void main(String[] args) {
+        int[] arr = {12,434,24,324,5456,466666,2710};//5
+        System.out.println(findEvenCount(arr));
+    }
+
+    static int findEvenCount(int[] arr){
+        int count = 0;
+        for( int a : arr){
+            if(isEvenDigit(a)){
+                count++;
+            }
+        };
+        return count;
+    };
+
+    static boolean isEvenDigit(int num){
+        int count = 0;
+        while (num>0){
+            count++;
+            num = num/10;
+        }
+        if(count % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+}
