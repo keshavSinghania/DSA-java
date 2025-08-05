b91f3e15a64198cd874a56667ceaca4b2b6fff41
Keshav Singhania <worksinghania@gmail.com>
2025-08-05 14:09:13 +0530

    BS gpt1

---
+++ src/day7_BInarySearchChatGpt/GreatestLowestNumber.java
@@ -0,0 +1,28 @@
+public class StrictlyGreaterElement {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 7, 11, 13, 17};
+        int target = 17; // try 13, 1, 18 etc.
+
+        int result = findStrictlyGreater(arr, target);
+        System.out.println(result);
+    }
+
+    static int findStrictlyGreater(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > target) {
+                ans = arr[mid];     // possible answer, but let's find smaller one
+                end = mid - 1;
+            } else {
+                start = mid + 1;    // move right if arr[mid] <= target
+            }
+        }
+
+        return ans;
+    }
+}
