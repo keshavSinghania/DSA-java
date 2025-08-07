8ea558098060a36bad238d869d1ee99bb1c03551
Keshav Singhania <worksinghania@gmail.com>
2025-08-07 21:31:45 +0530

    Find the Smallest Divisor Given a Threshold

---
+++ src/day8_StriverBinarySearchQuestions/FindtheSmallestDivisorGivenaThreshold.java
@@ -0,0 +1,10 @@
+package day8_StriverBinarySearchQuestions;
+
+public class FindtheSmallestDivisorGivenaThreshold {
+    public static void main(String[] args) {
+        return;
+    }
+}
+
+//Find the Smallest Divisor Given a Threshold
+//https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/description/
