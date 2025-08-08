cc9e794f83e58d8bca889865683f71c9b271c99d
Keshav Singhania <worksinghania@gmail.com>
2025-08-08 16:48:40 +0530

    1539. Kth Missing Positive Number

---
+++ src/day9_StriverBinarySearchQuestions/KthMissingPositive.java
@@ -0,0 +1,13 @@
+
+//https://leetcode.com/problems/kth-missing-positive-number/
+
+//class Solution {
+//    public int findKthPositive(int[] arr, int k) {
+//        for(int i=0; i<=arr.length-1; i++){
+//            if(arr[i] < k){
+//                k++;
+//            }
+//        }
+//        return k;
+//    }
+//}
